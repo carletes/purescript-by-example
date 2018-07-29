76c4d1fe26485db22b2fc3c6ad125e46167c778c
Carlos Valiente <carlos@pepelabs.net>
2018-07-29 12:55:31 +0100

    Chapter 2.

---
+++ 02-getting-started/my-project/src/Main.purs
@@ -0,0 +1,20 @@
+module Main where
+
+import Math (pi, sqrt)
+import Prelude
+import Effect (Effect)
+import Effect.Console (log, logShow)
+
+diagonal ∷ Number → Number → Number
+diagonal w h = sqrt (w * w + h * h)
+
+-- Exercise 1: `circleArea`
+
+circleArea ∷ Number → Number
+circleArea r = pi * r * r
+
+main :: Effect Unit
+main = do
+  log "Hello sailor!"
+  logShow (diagonal 3.0 4.0)
+  logShow (circleArea 42.0)
